587ac4bab03aef7a4f5749298267f3ce08aa2468
Asad8687 <asadm8687@gmail.com>
2026-01-06 21:06:00 +0100

    Added code for logger

---
+++ app/logger.py
@@ -0,0 +1,14 @@
+import logging
+from app.config import LOG_FILE
+
+def get_logger():
+    logger = logging.getLogger("budget")
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        handler = logging.FileHandler(LOG_FILE)
+        formatter = logging.Formatter("%(levelname)s: %(message)s")
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    return logger
